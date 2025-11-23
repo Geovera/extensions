89d758f9408bcafd23623069ff21acb390d8e396
Geovanny Vera <31142805+Geovera@users.noreply.github.com>
2025-11-23 01:23:05 -0500

    Update sukebei.js

--- sukebei.js
+++ sukebei.js
@@ -24,6 +24,8 @@ export default new class sukebei extends AbstractSource {
         headers.append('Access-Control-Allow-Origin', 'https://sukebei.nyaa.si');
 
         const link = encodeUriComponent(`${this.url}?f=0&c=1_0&q=${titles[0]}&p=1`)
+        console.log('Hello world')
+        console.log(link)
         const res = await fetch(`https://api.allorigins.win/get?url=${link}`,
                                 {
                                  headers: headers   
